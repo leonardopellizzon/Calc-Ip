790438a39cf84a53b854b26d0bda82dd7b4b267c
leonardopellizzon <72733218+leonardopellizzon@users.noreply.github.com>
2022-09-26 15:40:39 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+require('dotenv').config();
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  pwa: {
+    name: 'Calc Ip',
+    themeColor: "#ffffff",
+  }
+}
