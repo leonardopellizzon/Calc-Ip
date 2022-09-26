9fa258c1e0e5f060ce16404f73114e9d7cef39ba
leonardopellizzon <72733218+leonardopellizzon@users.noreply.github.com>
2022-09-26 16:24:24 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,89 @@
 <template>
   <div class="m-5">
-    <input
-      type="text"
-      class="form-control text-center mb-2"
-      placeholder="Indirizzo"
-      aria-label="Username"
-      v-model="indirizzo"
-    />
-    <input
-      type="text"
-      class="form-control text-center mb-2"
-      placeholder="Prefisso"
-      aria-label="Server"
-      v-model="prefisso"
-    />
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="richiedi(indirizzo,prefisso)">
-      Calcola
+    <div class="modal fade text-brown" id="calcola">
+      <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title"><strong>Calcola ip</strong></h4>
+          </div>
+
+          <div class="modal-body">
+            <input
+              type="text"
+              class="form-control text-center mb-2"
+              placeholder="Indirizzo"
+              aria-label="Username"
+              v-model="indirizzo"
+            />
+            <input
+              type="text"
+              class="form-control text-center"
+              placeholder="Prefisso"
+              aria-label="Server"
+              v-model="prefisso"
+            />
+          </div>
+
+          <div class="modal-footer d-flex justify-content-center">
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+              data-bs-toggle="modal"
+              data-bs-target="#calcola"
+              @click="richiedi(indirizzo, prefisso)"
+            >
+              Calcola
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button
+      type="button"
+      class="btn btn-dark btn-sm text-center mb-4"
+      data-bs-toggle="modal"
+      data-bs-target="#calcola"
+    >
+      Inserisci dati
     </button>
-  <div class="table-responsive-xl  rounded">
-    <table class="table table-dark table-striped">
-      <tbody>
-        <tr class="table-danger">
-          <th scope="row">Indirizzo analizzato</th>
-          <td>{{ indirizzo_cidr }}</td>
-        </tr>
-        <tr class="table-success">
-          <th scope="row">Subnet mask</th>
-          <td>{{ subnet }}</td>
-        </tr>
-        <tr class="table-primary">
-          <th scope="row">Broadcast</th>
-          <td>{{ broadcast }}</td>
-        </tr>
-        <tr class="table-primary">
-          <th scope="row">ID rete</th>
-          <td>{{ idRete }}</td>
-        </tr>
-        <tr>
-          <th scope="row">Primo indirizzo disponibile</th>
-          <td>{{ primoInd }}</td>
-        </tr>
-        <tr>
-          <th scope="row">Ultimo indirizzo disponibile</th>
-          <td>{{ ultimoInd }}</td>
-        </tr>
-        <tr class="table-warning">
-          <th scope="row">Host totali</th>
-          <td>{{ numeroHost }}</td>
-        </tr>
-      </tbody>
-    </table>
+
+    <div class="table-responsive-xl rounded">
+      <table class="table table-dark table-striped">
+        <tbody>
+          <tr class="table-danger">
+            <th scope="row">Indirizzo analizzato</th>
+            <td>{{ indirizzo_cidr }}</td>
+          </tr>
+          <tr class="table-success">
+            <th scope="row">Subnet mask</th>
+            <td>{{ subnet }}</td>
+          </tr>
+          <tr class="table-primary">
+            <th scope="row">Broadcast</th>
+            <td>{{ broadcast }}</td>
+          </tr>
+          <tr class="table-primary">
+            <th scope="row">ID rete</th>
+            <td>{{ idRete }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Primo indirizzo disponibile</th>
+            <td>{{ primoInd }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Ultimo indirizzo disponibile</th>
+            <td>{{ ultimoInd }}</td>
+          </tr>
+          <tr class="table-warning">
+            <th scope="row">Host totali</th>
+            <td>{{ numeroHost }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -74,9 +106,9 @@ export default {
   mounted() {},
   methods: {
     richiedi(indirizzo, prefisso) {
-      if(indirizzo == undefined || prefisso == undefined) return
+      if (indirizzo == undefined || prefisso == undefined) return;
       this.axios
-        .get("https://networkcalc.com/api/ip/"+indirizzo+"/" + prefisso)
+        .get("https://networkcalc.com/api/ip/" + indirizzo + "/" + prefisso)
         .then((res) => {
           console.log(res.data.address);
           this.data = res.data;
